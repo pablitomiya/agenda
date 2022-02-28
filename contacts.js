df092b777b851f77c18ad5919ae169314983a295
pablitomiya <pablitomiya@gmail.com>
2022-02-28 17:06:18 -0300

    module contacts

---
+++ contacts.js
@@ -0,0 +1,19 @@
+const contacts = [
+    {
+        id:1,
+        name: "pablo",
+        number: 3043800
+    },
+    {
+        id:2,
+        name: "jose",
+        number: 3043801
+    },
+    {
+        id:3,
+        name: "pepe",
+        number: 3043802
+    },
+    
+]
+module.exports = contacts
