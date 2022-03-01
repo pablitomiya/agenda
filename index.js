612651bfe027802cb235b3f5a1429ac1f5a78699
pablitomiya <pablitomiya@gmail.com>
2022-03-01 14:13:23 -0300

    more refactoring

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ app.use(
         )
     )
 
-
-
-// data
-//const persons = require('./contacts')
-
 // get utils
 // const { midLogger } = require('./utils')
 
@@ -33,55 +28,7 @@ app.get('/', (request, response)=> {
     response.send('<h1>API agenda</h1>')
 })
 
-// //get persons/id
-// app.get('/api/persons/:id', (request,response) => {
-//     // const id = request.params.id
-//     const { id } = request.params
-//     // const note = notes.find((item, idx) => {
-//     //     if (item.id == id) return item
-//     // })
-//     const person = persons.find(item => item.id === Number(id))
-//     //si la nota no existe deberemos devolver 404
-//     if (person) {
-//         response.json(person)
-//     } else {
-//         response.status(404).end()
-//     }
-// })
 
-
-// //get persons
-// app.get('/api/persons', (request,response)=>{
-//     response.json(persons)
-// })
-
-// //delete person
-// app.delete('/api/persons/:id', (request,response) => {
-//     const { id } = request.params
-//     const resultPersons = persons.filter(item => item.id !== Number(id))
-//     // console.log(resultPersons)
-//     response.status(204).end()
-// })
-
-
-// app.post('/api/persons/', (request,response) => {
-//     const newPerson = request.body
-//     const resultPersons = persons.concat(newPerson)
-//     // console.log(resultPersons)
-//     response.status(201).json(newPerson)
-
-// })
-
-// app.put('/api/persons/:id', (request,response) => {
-//     const { id } = request.params
-//     const data = request.body
-//     const person = persons.find(item => item.id === Number(id))
-//     if (person) {
-//         response.json(person)
-//     } else {
-//         response.status(404).end()
-//     }
-// })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
