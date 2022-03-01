0f96d667641199d40afa7cacd9d293c1609a46dc
pablitomiya <pablitomiya@gmail.com>
2022-03-01 10:31:16 -0300

    port corriged

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.put('/api/persons/:id', (request,response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 
